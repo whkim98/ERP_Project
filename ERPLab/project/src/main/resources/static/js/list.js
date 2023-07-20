dd67b990bdb1128b0a881a3b0601d86bc9a3756e
JNjeong <wnsghd0902@gmail.com>
2023-07-20 21:33:16 +0900

    old version delete
    old version delete and re-launce program with correct versioning

--- ERPLab/project/src/main/resources/static/js/list.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $("name=pageRows").change(function(){
-//        alert(${this}.val()); //log용
-        var frm = $("name='frmPageRows']")
-        frm.attr("method", "POST")
-        frm.attr("action", "pageRows")
-        frm.submit();
-    });
-});
