21b98df128285e2d2d3c1bad87b007a3c190c7b1
MichealnotJackson <tlsehdgus007@naver.com>
2023-07-24 19:50:28 +0900

    login controller
    로그인 컨트롤러 및 부가적인 로직 작성

--- workspace/ERPlab/src/main/java/com/lab/erp/service/all/LoginService.java
+++ workspace/ERPlab/src/main/java/com/lab/erp/service/all/LoginService.java
@@ -1,9 +1,13 @@
 package com.lab.erp.service.all;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.lab.erp.dao.all.Erp_AdminDAO;
 import com.lab.erp.dao.all.Erp_EmployeeDAO;
+import com.lab.erp.vo.all.Erp_AdminVO;
 
 public class LoginService {
 	private Erp_EmployeeDAO edao;
@@ -15,5 +19,27 @@ public class LoginService {
 		this.adao = adao;
 	}
 	
+	public int checkAdmin(Map<String, Object> map) {
+		return adao.checkAdmin(map);
+	}
 	
+	public List<Erp_AdminVO> findAdminId(int team_no){
+		return adao.findAdminId(team_no);
+	}
+	
+	public Erp_AdminVO findAdminPw(Map<String, Object> map) {
+		return adao.findAdminPw(map);
+	}
+	
+	public int checkEmp(Map<String, Object> map) {
+		return edao.checkEmp(map);
+	}
+	
+	public String findEmpId(Map<String, Object> map) {
+		return edao.findEmpId(map);
+	}
+	
+	public String findEmpPw(Map<String, Object> map) {
+		return edao.findEmpPw(map);
+	}
 }
