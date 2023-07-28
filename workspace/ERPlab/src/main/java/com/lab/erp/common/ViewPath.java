e6044f341d16fb56ea778ab24954b91b991d3fce
MichealnotJackson <tlsehdgus007@naver.com>
2023-07-28 16:21:37 +0900

    기존 프로젝트 삭제 후 다시 설정

--- workspace/ERPlab/src/main/java/com/lab/erp/common/ViewPath.java
+++
@@ -1,8 +0,0 @@
-package com.lab.erp.common;
-
-public class ViewPath {
-	public static final String LOGIN = "login/";
-	public static final String RESULT = "result/";
-	public static final String WINDOW = "window/";
-	
-}
