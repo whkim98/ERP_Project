c32ae65e3f73ae90f2662b3b9c64099b0575d20b
JNjeong <wnsghd0902@gmail.com>
2023-08-03 17:55:56 +0900

    a1 update
    a1 update

--- workspace/ERPlab/src/main/java/com/lab/erp/service/a/A1Service.java
+++ workspace/ERPlab/src/main/java/com/lab/erp/service/a/A1Service.java
@@ -1,5 +1,25 @@
 package com.lab.erp.service.a;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Service;
+
+import com.lab.erp.repository.a1.A1Repository;
+import com.lab.erp.vo.a.a1.Erp_CompanyVO;
+
+@Service
 public class A1Service {
 
+	private A1Repository a1Repository;
+	
+	public A1Service(SqlSession sqlSession) {
+		System.out.println("### LOG : " + getClass().getName()+"() 생성");
+		a1Repository = sqlSession.getMapper(a1Repository.getClass());
+	}
+	
+	// 전체 회사 정보 출력
+	public List<Erp_CompanyVO> list() {return a1Repository.findAll();}
+	
+	
 }
