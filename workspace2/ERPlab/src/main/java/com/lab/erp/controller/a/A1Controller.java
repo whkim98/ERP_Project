1c4fbfc1fe04e5a8e9b797115aaa4fa204d8f74c
JNjeong <wnsghd0902@gmail.com>
2023-08-11 04:51:40 +0900

    company function update
    company function update

--- workspace2/ERPlab/src/main/java/com/lab/erp/controller/a/A1Controller.java
+++ workspace2/ERPlab/src/main/java/com/lab/erp/controller/a/A1Controller.java
@@ -6,10 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.lab.erp.service.a.A1Service;
+import com.lab.erp.vo.a.a1.Erp_BusinesstypeVO;
 import com.lab.erp.vo.a.a1.Erp_CompanyVO;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/a")
@@ -26,10 +32,23 @@ public class A1Controller {
 		System.out.println("### LOG: "+ getClass().getName() + "() 생성");
 	}
 	
-	@GetMapping("/company")
-	public String a_company(Model model) {
-		List<Erp_CompanyVO> list_res = a1Service.list();
-		model.addAttribute("list", list_res);
+	@GetMapping("/a_company")
+	public String a_company(Model model ) {
+		List<Erp_CompanyVO> comp_list = a1Service.list();
+		List<Erp_BusinesstypeVO> bst_list = a1Service.bstlist();
+		model.addAttribute("comp_list", comp_list);
+		model.addAttribute("bst_list", bst_list);
+		System.out.println("get a_company 이동");
 		return "thymeleaf/a/a_company";
+	}
+	
+	@PostMapping("/a_company")
+	public String a_company(HttpSession httpSession, @ModelAttribute("comp_info")Erp_CompanyVO erp_CompanyVO) {
+		//TODO : 1 말고 httpSession 값에 있는 comcode_no으로 변경할것
+		erp_CompanyVO.setComcode_no(1); // temporary
+		a1Service.save(erp_CompanyVO);
+		
+		System.out.println("post a_company 이동");
+		return "redirect:/a/a_company";
 	}
 }
--- workspace2/ERPlab/src/main/resources/static/js/a/a_company.js
+++ workspace2/ERPlab/src/main/resources/static/js/a/a_company.js
@@ -1,25 +1,58 @@
+// 변수 초기화
+let wrapper = document.getElementById("wrapper");
+
 // 사업형태 조회 버튼
 let businesstype_search_btn = document.getElementById("businesstype_search");
 
-// 업종코드목록조회 열기 버튼
-businesstype_search_btn.addEventListener("click", function(){
-	document.getElementById("wrapper").style.display="block";
-	console.log("### LOG : businesstype search");
+// 업종코드목록조회 열기 버튼 기능 추가
+businesstype_search_btn.addEventListener('click', () => {
+	wrapper.style.display="block";
+	console.log("### LOG : open businesstype list");
 });
+
+// 닫기 버튼
+let close_businesstype = document.getElementsByClassName("close_businesstype")[0];
+close_businesstype.addEventListener('click', () => {
+	businesstype_close_func(); 
+});
+
+// 닫기 기능
+let businesstype_close_func = () => {
+	wrapper.style.display="none";
+}
 
 // 업종코드목록조회 선택버튼
 let businesstype_select_btn = document.getElementsByClassName("comp_type_select_btn");
 
-// 업종코드목록 선택버튼
-businesstype_select_btn.addEventListender("click", function(){
-	//TODO
-});
-
-// 닫기 버튼
-let close_businesstype = document.getElementsByClassName("close_businesstype");
-close_businesstype.addEventListener("click", businesstype_close_func);
-
-// 닫기 기능
-let businesstype_close_func = () => {
-		document.getElementById("wrapper").style.display="none";
+for(let i = 0 ; i < businesstype_select_btn.length ; i++) {
+	// 업종코드목록조회 선택버튼 기능추가
+	businesstype_select_btn[i].addEventListener('click', (e) => {
+		let ctgr = e.target.parentElement.parentElement.parentElement.parentElement.children[0].children[0].children[1].innerHTML;	
+		document.getElementById("company_businesstype").value = ctgr.trimStart().split('&gt;')[0].trimEnd();
+		let bstno = e.target.parentElement.parentElement.parentElement.parentElement.children[0].children[0].children[2].innerHTML;
+		document.getElementById("businesstype_no").value = bstno;
+		businesstype_close_func();
+	});	
 }
+
+// 회사 리스트 클릭시 정보출력
+let comp_items = document.getElementsByClassName("comp_items");
+for(let i = 0 ; i < comp_items.length; i++){
+	comp_items[i].addEventListener('click', (e) => {
+		//reset(comp_items, comp_items.length, "comp_items");
+		//e.target.parentElement.className = "comp_items selected_comp_items";
+		
+		location.href="/a/a_company?id="+e.target.parentElement.children[0].innerHTML;
+	});	
+}
+
+// 회사 리스트 클릭시 기타 회사는 표시 제거
+/*
+let reset = (taglist, length, className)=>{
+	for(let i = 0 ; i < length; i++){
+		taglist[i].className = className;
+	}
+}
+ */
+
+
